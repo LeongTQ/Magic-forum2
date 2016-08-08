30de8bd7d6a5f7f0681de9947fef7c46b812d626
Quin <leongtsuquin@gmail.com>
2016-08-08 17:33:26 +0800

    Add images to posts & comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   has_many :comments
-  belongs_to :topic, optional:true
-
+  belongs_to :topic
+  mount_uploader :image, ImageUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-root "landing#index"
-get :about, to: 'static_pages#about'
-resources :topics
-resources :posts 
+  root "landing#index"
+  get :about, to: 'static_pages#about'
+  resources :topics, except: [:show] do
+    resources :posts, except: [:show] do
+      resources :comments, except: [:show]
+    end
+  end
+
 end
